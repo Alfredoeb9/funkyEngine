7e4413fb9d8d1ca10369cc94bc309dfe595b0045
Alfredoeb9 <alfredoeb96@gmail.com>
2026-04-21 10:38:14 -0700

    [Input Manager] renmaed isKeyPressed and created proper isKeyPressed function
    * renamed isKeyPressed to isKeyDown and created proper isKeyPressed that tracks when a key is pressed once

--- funkyEngine/core/InputManager.cpp
+++ funkyEngine/core/InputManager.cpp
@@ -8,13 +8,25 @@ FunkyEngine::InputManager::~InputManager() {
 
 }
 
+void FunkyEngine::InputManager::updateKeyMaps() {
+    for (auto& key : _keyPressedMap) {
+        // If the key was pressed in the current frame, set it to true in the key map
+        if (key.second) {
+            _keyMap[key.first] = true;
+        }
+    }
+}
+
+
 void FunkyEngine::InputManager::pressKey(unsigned int keyID) {
     // First key if this key ID is in the map and if its not then set it to true
+    _keyPressedMap[keyID] = true;
     _keyMap[keyID] = true;
 }
 
 void FunkyEngine::InputManager::releaseKey(unsigned int keyID) {
     _keyMap[keyID] = false;
+    _keyPressedMap[keyID] = false;
 }
 
 void FunkyEngine::InputManager::setMouseCoords(float x, float y) {
@@ -23,12 +35,16 @@ void FunkyEngine::InputManager::setMouseCoords(float x, float y) {
 }
 
 bool FunkyEngine::InputManager::isKeyPressed(unsigned int keyID) {
-    auto iter = _keyMap.find(keyID);
+    auto iter = _keyPressedMap.find(keyID);
 
     // if it did find the key
-    if (iter != _keyMap.end()) {
+    if (iter != _keyPressedMap.end()) {
         return iter->second;
     } else {
         return false;
     }
+}
+
+bool FunkyEngine::InputManager::isKeyDown(unsigned int keyID) {
+    return isKeyPressed(keyID) || _keyMap[keyID];
 }
--- funkyEngine/core/InputManager.h
+++ funkyEngine/core/InputManager.h
@@ -5,15 +5,32 @@
 
 namespace FunkyEngine {
     class InputManager {
+        private:
+            std::unordered_map<unsigned int, bool> _keyPressedMap; // Map to track keys that were pressed in the current frame
+            std::unordered_map<unsigned int, bool> _keyMap;         // Map to track the current state of keys (pressed or not)
+            glm::vec2 _mouseCoords;
+
+
         public:
             InputManager();
             ~InputManager();
 
+            void updateKeyMaps(); // Helper function to update key maps at the end of each frame
+
             // Fix (abstracts): wrap all of the STL keys in own enum class
             //                  to give the ability to pick the bindings myself incase we use GLFW insead of SDL
+            
+            // 
             void pressKey(unsigned int keyID);
+
+            // Tells the input manager that a key was released
             void releaseKey(unsigned int keyID);
+
+            // Returns true if the key was pressed in the current frame
             bool isKeyPressed(unsigned int keyID);
+            
+            // returns true if the key is currently down (held down or just pressed)
+            bool isKeyDown(unsigned int keyID);
 
             // Setter
             void setMouseCoords(float x, float y);
@@ -21,9 +38,6 @@ namespace FunkyEngine {
             // Getters
             glm::vec2 getMouseCoords() const { return _mouseCoords; }
 
-        private:
-
-            std::unordered_map<unsigned int, bool> _keyMap;
-            glm::vec2 _mouseCoords;
+        
     };
 };
